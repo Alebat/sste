1a4afbfc22a4de8c688443fc6431df59d8438c3b
AleBat <alebat25@yahoo.it>
2015-06-22 11:00:08 +0200

    Ciao Ciao:D

---
+++ Boge/EventsMan.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WS_STE
+{
+    /// <summary>
+    /// Gestisce gli eventi per il triggering.
+    /// </summary>
+    public abstract class EventsMan
+    {
+        public delegate string TriggerEventHandler(EventsMan sender, object trigger, int v1 = -1, int v2 = -1);
+
+        volatile bool _started = false;
+        volatile bool _ended = false;
+        Thread _process = null;
+        SemaphoreSlim _sem = new SemaphoreSlim(0, 1);
+
+        public event TriggerEventHandler Trigs;
+
+        public object CurrentEvent { get; protected set; }
+
+        ~EventsMan()
+        {
+            Close();
+        }
+
+        protected string Trig(object evt, int t = -1)
+        {
+            CurrentEvent = evt;
+            if (Trigs != null)
+                return Trigs.Invoke(this, evt, t, -1);
+            return null;
+        }
+
+        protected void Sleep(int millis = -1)
+        {
+            if (millis < 0)
+                _sem.Wait();
+            else
+                _sem.Wait(millis);
+        }
+
+        internal void End()
+        {
+            _ended = true;
+        }
+
+        public void Reset()
+        {
+            Close();
+            _process = new Thread(new ThreadStart(Process));
+            _started = false;
+            _ended = false;
+        }
+
+        public void Start()
+        {
+            if (!_started)
+            {
+                if (_process == null)
+                    Reset();
+                _process.Start();
+                _started = true;
+            }
+        }
+
+        public void Close()
+        {
+            if (_process != null)
+            {
+                if (_process.IsAlive)
+                    _process.Abort();
+                _process = null;
+            }
+        }
+
+        public void Resume(object controlCurrentEvent)
+        {
+            if (CurrentEvent.Equals(controlCurrentEvent))
+                _sem.Release();
+        }
+
+        protected abstract void Process();
+    }
+}
---
+++ Boge/Yagmur6Events.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace WS_STE
+{
+
+    public class Yagmur6EventsMan : EventsMan
+    {
+        public class Event
+        {
+            /// <param name="duration">0 for invisible, negative for wait resume, positive for timeout.</param>
+            public Event(string name, int duration)
+            {
+                Name = name;
+                Duration = duration;
+            }
+
+            public string Name { get; protected set; }
+            public int Duration { get; protected set; }
+            public bool Invisible { get { return Duration == 0; } }
+        }
+
+        public enum Yagmur6Event
+        {
+            Message, Fix, Sound, Rest, Break, Rating, End
+        }
+
+        public const int PracticeSound = 0;
+        public const int FirstBlockSound = 1;
+        public const int SecondBlockSound = 2;
+
+        int _first, _second, _practice;
+        int _breakDuration;
+
+        Dictionary<Yagmur6Event, Event> _loaded;
+
+        public Yagmur6EventsMan(int practiceBlockCycles, int firstBlockCycles, int secondBlockCycles, Dictionary<Yagmur6Event, Event> loaded)
+        {
+            _first = firstBlockCycles;
+            _second = secondBlockCycles;
+            _practice = practiceBlockCycles;
+            _loaded = loaded;
+        }
+
+        protected override void Process()
+        {
+            // Instructions
+            Instructions(1);
+            // Practice
+            Second(true);
+            // Instructions
+            Instructions(3);
+            // First
+            First();
+            // Break
+            Break();
+            //Instructions
+            Instructions(3);
+            // Second
+            Second();
+            // End
+            End();
+        }
+
+        private void WaitLoaded(Yagmur6Event e, int p = -1)
+        {
+            Trig(e, p);
+            if (_loaded.ContainsKey(e))
+                base.Sleep(_loaded[e].Duration);
+        }
+
+        private void Instructions(int p)
+        {
+            WaitLoaded(Yagmur6Event.Message, p);
+        }
+
+        private void Break()
+        {
+            WaitLoaded(Yagmur6Event.Break);
+        }
+
+        private void First()
+        {
+            for (int i = 0; i < _first; i++)
+            {
+                WaitLoaded(Yagmur6Event.Fix);
+                WaitLoaded(Yagmur6Event.Sound, FirstBlockSound);
+                WaitLoaded(Yagmur6Event.Rest);
+            }
+        }
+
+        private void Second(bool practice = false)
+        {
+            for (int i = 0; i < _second; i++)
+            {
+                WaitLoaded(Yagmur6Event.Fix);
+                WaitLoaded(Yagmur6Event.Sound, practice ? PracticeSound : SecondBlockSound);
+                WaitLoaded(Yagmur6Event.Rating);
+            }
+        }
+
+        private new void End()
+        {
+            WaitLoaded(Yagmur6Event.End);
+            base.End();
+        }
+    }
+}
